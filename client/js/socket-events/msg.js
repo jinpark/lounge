d42e5a949ba88e4a72ddb04e027f7c8cc6898a1b
Jin Park <jin@jinpark.net>
2017-11-16 15:01:18 +0900

    add highmon

--- client/js/socket-events/msg.js
+++ client/js/socket-events/msg.js
@@ -101,6 +101,16 @@ function notifyMessage(targetId, channel, msg) {
 
 	const button = sidebar.find(".chan[data-id='" + targetId + "']");
 	if (msg.highlight || (options.notifyAllMessages && msg.type === "message")) {
+		// append to highmon
+		const container = $("#highmon .messages");
+		targetId = $("#highmon");
+		render.appendMessage(
+			container,
+			targetId,
+			'highmon',
+			msg
+		);
+		console.log(['this is the notification message', msg]);
 		if (!document.hasFocus() || !channel.hasClass("active")) {
 			if (options.notification) {
 				try {
